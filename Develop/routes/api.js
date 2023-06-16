2f4c81c22baca2e91a48a3a0c8a6e11c53056864
Adam Colin <accolin89@gmail.com>
2023-06-15 19:54:23 -0500

    getting the helper folder with fsUtils.js file to work

--- Develop/routes/api.js
+++ Develop/routes/api.js
@@ -1,7 +1,43 @@
 const api = require('express').Router()
+const { readFromFile, writeToFile, readAndAppend } = require('../helpers/fsUtils')
 
 
 
-api.get('/notes', (req, res) => res.json(database));
+api.get('/notes', (req, res) => {
+    console.info(`${req.method} request received for notes`);
+  
+    readFromFile('./Develop/db/db.json').then((data) => res.json(JSON.parse(data)));
+  });
+
+  api.post('/', (req, res) => {
+    // Log that a POST request was received
+    console.info(`${req.method} request received to submit feedback`);
+  
+    // Destructuring assignment for the items in req.body
+    const { email, feedbackType, feedback } = req.body;
+  
+    // If all the required properties are present
+    if (email && feedbackType && feedback) {
+      // Variable for the object we will save
+      const newFeedback = {
+        email,
+        feedbackType,
+        feedback,
+        feedback_id: uuid(),
+      };
+  
+      readAndAppend(newFeedback, './db/feedback.json');
+  
+      const response = {
+        status: 'success',
+        body: newFeedback,
+      };
+  
+      res.json(response);
+    } else {
+      res.json('Error in posting feedback');
+    }
+  });
+
 
 module.exports = api;
--- Develop/routes/notes.js
+++ Develop/routes/notes.js
@@ -3,13 +3,13 @@ const notes = require('express').Router();
 
 
 notes.get('/home', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
+  res.sendFile(path.join(__dirname, '../public/index.html'))
 );
 
 
 
 notes.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/notes.html'));
+    res.sendFile(path.join(__dirname, '../public/notes.html'));
   });
 
 //this allows us to access this file when we use it on other files like server.js
