cb8e5ff8b1c534067e6e1c02ce217fd69c5ed11e
Adam Colin <accolin89@gmail.com>
2023-06-15 20:24:59 -0500

    created get/post and now working on methods for the class

--- Develop/routes/api.js
+++ Develop/routes/api.js
@@ -5,39 +5,39 @@ const { readFromFile, writeToFile, readAndAppend } = require('../helpers/fsUtils
 
 api.get('/notes', (req, res) => {
     console.info(`${req.method} request received for notes`);
-  
-    readFromFile('./Develop/db/db.json').then((data) => res.json(JSON.parse(data)));
-  });
 
-  api.post('/', (req, res) => {
+    readFromFile('./Develop/db/db.json').then((data) => res.json(JSON.parse(data)));
+});
+
+api.post('/notes', (req, res) => {
     // Log that a POST request was received
-    console.info(`${req.method} request received to submit feedback`);
-  
+    console.info(`${req.method} request received to submit notes`);
+
     // Destructuring assignment for the items in req.body
     const { email, feedbackType, feedback } = req.body;
-  
+
     // If all the required properties are present
     if (email && feedbackType && feedback) {
-      // Variable for the object we will save
-      const newFeedback = {
-        email,
-        feedbackType,
-        feedback,
-        feedback_id: uuid(),
-      };
-  
-      readAndAppend(newFeedback, './db/feedback.json');
-  
-      const response = {
-        status: 'success',
-        body: newFeedback,
-      };
-  
-      res.json(response);
+        // Variable for the object we will save
+        const newFeedback = {
+            email,
+            feedbackType,
+            feedback,
+            feedback_id: uuid(),
+        };
+
+        readAndAppend(newFeedback, './db/db.json');
+
+        const response = {
+            status: 'success',
+            body: newFeedback,
+        };
+
+        res.json(response);
     } else {
-      res.json('Error in posting feedback');
+        res.json('Error in posting feedback');
     }
-  });
+});
 
 
 module.exports = api;
