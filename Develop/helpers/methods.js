98454c952e5137c90d7c37fe574e15651e2c3083
Adam Colin <accolin89@gmail.com>
2023-06-16 22:13:01 -0500

    rewrote code for the read, write, getNotes, and postNote function using the async/await syntax we learned in class yesterday and with my tutor. provided notes as well to help better understand how that works

--- Develop/helpers/methods.js
+++ Develop/helpers/methods.js
@@ -6,12 +6,31 @@ const readFile = util.promisify(fs.readFile);
 
 const writeFile = util.promisify(fs.writeFile);
 
-class Methods {
+class Methods { //making this class asynchronous by using async function. when it gets to await, it's going to pause and execute the readFile function, which will read the data from the db.json file and parse it into the array
     async read() {
         const data = await readFile('.db/db.json', 'utf8');
         return JSON.parse(data) || [];
     }
+    //this will write the data from the user inputs into the db.json and then pause until that's done (because fo the await function)
+    async write(data) {
+        await writeFile("./db/db.json", JSON.stringify(data));
+    }
+
+    //getNotes function is tapping into the above read function to get the notes that are in the db.json file, but if there aren't any notes it will return an empty array
+    async getNotes() {
+        const data = await this.read();
+        return Array.isArray(data) ? data : [];
+    }
+
+    //postNote allows the user to actually post a note to the note taker. the if statement is saying that if there's no text or no title, throw an error because there needs to be some input in order to post the note
+    async postNote(note) {
+        const { title, text } = note;
+        if (!title || !text ) {
+            throw new Error('Please enter a title and text for the note!')
+        }
+    }
 }
+
 
 
 class Methods {
